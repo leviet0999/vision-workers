f5f71fbcc9a4c0d216aad9674eea75d545ac96cc
leviet0999 <leviet099898@gmail.com>
2024-07-30 23:08:41 +0700

    Update checking_functions.py

--- validator_orchestrator/app/checking/checking_functions.py
+++ validator_orchestrator/app/checking/checking_functions.py
@@ -5,215 +5,215 @@ import json
 import random
 from app import utility_models
 from app.checking import utils as checking_utils
-import xgboost as xgb
+# import xgboost as xgb
 import math
 from loguru import logger
-from PIL import Image
+# from PIL import Image
 import os
 import io
-from PIL import UnidentifiedImageError
+# from PIL import UnidentifiedImageError
 from typing import Optional
 from app.constants import BASE_URL
 
 
-images_are_same_classifier = xgb.XGBClassifier()
-images_are_same_classifier.load_model("image_similarity_xgb_model.json")
+# images_are_same_classifier = xgb.XGBClassifier()
+# images_are_same_classifier.load_model("image_similarity_xgb_model.json")
 
 
-##### SOTA ######
+# ##### SOTA ######
 
 
-async def check_sota_result(
-    result: models.QueryResult, synapse: Dict[str, Any], task_config: models.TaskConfig
-) -> bool:
-    sota_result = utility_models.SotaResponse(**result.formatted_response)
+# async def check_sota_result(
+#     result: models.QueryResult, synapse: Dict[str, Any], task_config: models.TaskConfig
+# ) -> bool:
+#     sota_result = utility_models.SotaResponse(**result.formatted_response)
 
-    valid_gojourney_url = checking_utils.validate_gojourney_url(sota_result.image_url)
+#     valid_gojourney_url = checking_utils.validate_gojourney_url(sota_result.image_url)
 
-    if not valid_gojourney_url:
-        return 0
+#     if not valid_gojourney_url:
+#         return 0
 
-    image_bytes = await checking_utils.fetch_image_as_bytes(sota_result.image_url)
+#     image_bytes = await checking_utils.fetch_image_as_bytes(sota_result.image_url)
 
-    prompt: Optional[str] = synapse.get("prompt", None)
-    if prompt is None:
-        logger.warning(
-            f"Error when fetching image {sota_result.image_url}, can't parse the bytes into a PIL for some reason"
-        )
-        return 0
+#     prompt: Optional[str] = synapse.get("prompt", None)
+#     if prompt is None:
+#         logger.warning(
+#             f"Error when fetching image {sota_result.image_url}, can't parse the bytes into a PIL for some reason"
+#         )
+#         return 0
 
-    if not image_bytes:
-        logger.warning(
-            f"Error when fetching image {sota_result.image_url}, can't parse the bytes into a PIL for some reason"
-        )
-        return 0
+#     if not image_bytes:
+#         logger.warning(
+#             f"Error when fetching image {sota_result.image_url}, can't parse the bytes into a PIL for some reason"
+#         )
+#         return 0
 
-    try:
-        original_image = Image.open(io.BytesIO(image_bytes))
-    except UnidentifiedImageError:
-        logger.warning(
-            f"Error when fetching image {sota_result.image_url}, can't parse the bytes into a PIL for some reason"
-        )
+#     try:
+#         original_image = Image.open(io.BytesIO(image_bytes))
+#     except UnidentifiedImageError:
+#         logger.warning(
+#             f"Error when fetching image {sota_result.image_url}, can't parse the bytes into a PIL for some reason"
+#         )
 
-    width, height = original_image.size
-    image_1 = original_image.crop((0, 0, width // 2, height // 2))
-    image_2 = original_image.crop((width // 2, 0, width, height // 2))
-    image_3 = original_image.crop((0, height // 2, width // 2, height))
-    image_4 = original_image.crop((width // 2, height // 2, width, height))
+#     width, height = original_image.size
+#     image_1 = original_image.crop((0, 0, width // 2, height // 2))
+#     image_2 = original_image.crop((width // 2, 0, width, height // 2))
+#     image_3 = original_image.crop((0, height // 2, width // 2, height))
+#     image_4 = original_image.crop((width // 2, height // 2, width, height))
 
-    images = [image_1, image_2, image_3, image_4]
+#     images = [image_1, image_2, image_3, image_4]
 
-    clip_image_embeddings_response: utility_models.ClipEmbeddingsResponse = (
-        await query_endpoint_for_clip_response(
-            data={"image_b64s": [checking_utils.pil_to_base64(i) for i in images]},
-            endpoint=BASE_URL + "/clip-embeddings",
-        )
-    )
+#     clip_image_embeddings_response: utility_models.ClipEmbeddingsResponse = (
+#         await query_endpoint_for_clip_response(
+#             data={"image_b64s": [checking_utils.pil_to_base64(i) for i in images]},
+#             endpoint=BASE_URL + "/clip-embeddings",
+#         )
+#     )
 
-    image_embeddings = clip_image_embeddings_response.clip_embeddings
+#     image_embeddings = clip_image_embeddings_response.clip_embeddings
 
-    split_prompt = prompt.split("--")[0]
-    text_embedding_response: utility_models.ClipTextEmbeddingsResponse = (
-        await query_endpoint_for_clip_text_response(
-            data={"text_prompt": split_prompt},
-            endpoint=BASE_URL + "/clip-embeddings-text",
-        )
-    )
+#     split_prompt = prompt.split("--")[0]
+#     text_embedding_response: utility_models.ClipTextEmbeddingsResponse = (
+#         await query_endpoint_for_clip_text_response(
+#             data={"text_prompt": split_prompt},
+#             endpoint=BASE_URL + "/clip-embeddings-text",
+#         )
+#     )
 
-    text_embedding = text_embedding_response.text_embedding
+#     text_embedding = text_embedding_response.text_embedding
 
-    sims = []
-    for image_embedding in image_embeddings:
-        embedding_sim = checking_utils.get_clip_embedding_similarity(
-            image_embedding, text_embedding
-        )
-        sims.append(embedding_sim)
+#     sims = []
+#     for image_embedding in image_embeddings:
+#         embedding_sim = checking_utils.get_clip_embedding_similarity(
+#             image_embedding, text_embedding
+#         )
+#         sims.append(embedding_sim)
 
-    average_sim = sum(sims) / len(sims)
+#     average_sim = sum(sims) / len(sims)
 
-    # Found that 0.21 finds valid images >99.9% of the time, but stops invalid ones > 99% of the time too!
-    if average_sim > 0.21:
-        return 1
+#     # Found that 0.21 finds valid images >99.9% of the time, but stops invalid ones > 99% of the time too!
+#     if average_sim > 0.21:
+#         return 1
 
-    return 0
-
-
-### CLIP  ####
+#     return 0
 
 
-async def query_endpoint_for_clip_text_response(
-    endpoint: str, data: Dict[str, Any]
-) -> utility_models.ClipTextEmbeddingsResponse:
-    async with httpx.AsyncClient(timeout=10) as client:
-        logger.info(f"Querying : {endpoint}")
-        response = await client.post(endpoint, json=data)
-        return utility_models.ClipTextEmbeddingsResponse(**response.json())
+# ### CLIP  ####
 
 
-async def query_endpoint_for_clip_response(
-    endpoint: str, data: Dict[str, Any]
-) -> utility_models.ClipEmbeddingsResponse:
-    async with httpx.AsyncClient(timeout=10) as client:
-        logger.info(f"Querying : {endpoint}")
-        response = await client.post(endpoint, json=data)
-        return utility_models.ClipEmbeddingsResponse(**response.json())
+# async def query_endpoint_for_clip_text_response(
+#     endpoint: str, data: Dict[str, Any]
+# ) -> utility_models.ClipTextEmbeddingsResponse:
+#     async with httpx.AsyncClient(timeout=10) as client:
+#         logger.info(f"Querying : {endpoint}")
+#         response = await client.post(endpoint, json=data)
+#         return utility_models.ClipTextEmbeddingsResponse(**response.json())
 
 
-async def check_clip_result(
-    result: models.QueryResult, synapse: Dict[str, Any], task_config: models.TaskConfig
-) -> Union[float, None]:
-    clip_result = utility_models.ClipEmbeddingsResponse(**result.formatted_response)
-
-    expected_clip_response = await query_endpoint_for_clip_response(
-        task_config.endpoint, synapse
-    )
-
-    if expected_clip_response.clip_embeddings is None:
-        logger.error(f"For some reason Everything is none! {expected_clip_response}")
-        return None
-
-    total_sim = 0
-    for i in range(
-        min(
-            len(expected_clip_response.clip_embeddings),
-            len(clip_result.clip_embeddings),
-        )
-    ):
-        clip_embedding_similiarity = checking_utils.get_clip_embedding_similarity(
-            clip_result.clip_embeddings[i], expected_clip_response.clip_embeddings[i]
-        )
-        total_sim += clip_embedding_similiarity
-
-    normalised_sim = total_sim / max(
-        len(expected_clip_response.clip_embeddings), len(clip_result.clip_embeddings)
-    )
-
-    return int(normalised_sim > 0.999)
+# async def query_endpoint_for_clip_response(
+#     endpoint: str, data: Dict[str, Any]
+# ) -> utility_models.ClipEmbeddingsResponse:
+#     async with httpx.AsyncClient(timeout=10) as client:
+#         logger.info(f"Querying : {endpoint}")
+#         response = await client.post(endpoint, json=data)
+#         return utility_models.ClipEmbeddingsResponse(**response.json())
 
 
-##### Image #######
-async def query_endpoint_for_image_response(
-    endpoint: str, data: Dict[str, Any]
-) -> utility_models.ImageResponseBody:
-    async with httpx.AsyncClient(
-        timeout=60 * 2
-    ) as client:
-        logger.info(f"Querying : {endpoint}")
-        response = await client.post(endpoint, json=data)
-        logger.info(response.status_code)
-        return utility_models.ImageResponseBody(**response.json())
+# async def check_clip_result(
+#     result: models.QueryResult, synapse: Dict[str, Any], task_config: models.TaskConfig
+# ) -> Union[float, None]:
+#     clip_result = utility_models.ClipEmbeddingsResponse(**result.formatted_response)
+
+#     expected_clip_response = await query_endpoint_for_clip_response(
+#         task_config.endpoint, synapse
+#     )
+
+#     if expected_clip_response.clip_embeddings is None:
+#         logger.error(f"For some reason Everything is none! {expected_clip_response}")
+#         return None
+
+#     total_sim = 0
+#     for i in range(
+#         min(
+#             len(expected_clip_response.clip_embeddings),
+#             len(clip_result.clip_embeddings),
+#         )
+#     ):
+#         clip_embedding_similiarity = checking_utils.get_clip_embedding_similarity(
+#             clip_result.clip_embeddings[i], expected_clip_response.clip_embeddings[i]
+#         )
+#         total_sim += clip_embedding_similiarity
+
+#     normalised_sim = total_sim / max(
+#         len(expected_clip_response.clip_embeddings), len(clip_result.clip_embeddings)
+#     )
+
+#     return int(normalised_sim > 0.999)
 
 
-async def check_image_result(
-    result: models.QueryResult, synapse: Dict[str, Any], task_config: models.TaskConfig
-) -> Union[float, None]:
-    image_response_body = utility_models.ImageResponseBody(**result.formatted_response)
+# ##### Image #######
+# async def query_endpoint_for_image_response(
+#     endpoint: str, data: Dict[str, Any]
+# ) -> utility_models.ImageResponseBody:
+#     async with httpx.AsyncClient(
+#         timeout=60 * 2
+#     ) as client:
+#         logger.info(f"Querying : {endpoint}")
+#         response = await client.post(endpoint, json=data)
+#         logger.info(response.status_code)
+#         return utility_models.ImageResponseBody(**response.json())
 
-    if (
-        image_response_body.image_b64 is None
-        and image_response_body.clip_embeddings is None
-        and image_response_body.image_hashes is None
-        and image_response_body.is_nsfw is None
-    ):
-        logger.error(f"For some reason Everything is none! {image_response_body}")
-        return 0
 
-    expected_image_response = await query_endpoint_for_image_response(
-        task_config.endpoint, synapse
-    )
+# async def check_image_result(
+#     result: models.QueryResult, synapse: Dict[str, Any], task_config: models.TaskConfig
+# ) -> Union[float, None]:
+#     image_response_body = utility_models.ImageResponseBody(**result.formatted_response)
 
-    if expected_image_response.is_nsfw and image_response_body.is_nsfw:
-        return 1
+#     if (
+#         image_response_body.image_b64 is None
+#         and image_response_body.clip_embeddings is None
+#         and image_response_body.image_hashes is None
+#         and image_response_body.is_nsfw is None
+#     ):
+#         logger.error(f"For some reason Everything is none! {image_response_body}")
+#         return 0
 
-    if expected_image_response.clip_embeddings is None:
-        logger.error(f"For some reason Everything is none! {expected_image_response}")
-        return None
+#     expected_image_response = await query_endpoint_for_image_response(
+#         task_config.endpoint, synapse
+#     )
 
-    # Server got a result but you didn't!
-    elif (
-        image_response_body.image_b64 is None
-        and expected_image_response.image_b64 is not None
-    ):
-        return 0
+#     if expected_image_response.is_nsfw and image_response_body.is_nsfw:
+#         return 1
 
-    clip_embedding_similiarity = checking_utils.get_clip_embedding_similarity(
-        image_response_body.clip_embeddings, expected_image_response.clip_embeddings
-    )
-    hash_distances = checking_utils.get_hash_distances(
-        image_response_body.image_hashes, expected_image_response.image_hashes
-    )
+#     if expected_image_response.clip_embeddings is None:
+#         logger.error(f"For some reason Everything is none! {expected_image_response}")
+#         return None
 
-    probability_same_image_xg = images_are_same_classifier.predict_proba(
-        [hash_distances]
-    )[0][1]
+#     # Server got a result but you didn't!
+#     elif (
+#         image_response_body.image_b64 is None
+#         and expected_image_response.image_b64 is not None
+#     ):
+#         return 0
 
-    # MODEL has a very low threshold
-    if probability_same_image_xg > 0.01:
-        score = 1
-    else:
-        score = clip_embedding_similiarity**2
+#     clip_embedding_similiarity = checking_utils.get_clip_embedding_similarity(
+#         image_response_body.clip_embeddings, expected_image_response.clip_embeddings
+#     )
+#     hash_distances = checking_utils.get_hash_distances(
+#         image_response_body.image_hashes, expected_image_response.image_hashes
+#     )
 
-    return score
+#     probability_same_image_xg = images_are_same_classifier.predict_proba(
+#         [hash_distances]
+#     )[0][1]
+
+#     # MODEL has a very low threshold
+#     if probability_same_image_xg > 0.01:
+#         score = 1
+#     else:
+#         score = clip_embedding_similiarity**2
+
+#     return score
 
 
 ########### TEXT ###########
@@ -306,7 +306,8 @@ async def get_chat_data_validator_response(
     async for line in response.aiter_lines():
         line_formatted = line.split("data: ")[1].split("\n\n")[0]
         response_json = json.loads(line_formatted)
-        return models.ValidatorCheckingResponse(**response_json)
+        # return models.ValidatorCheckingResponse(**response_json)
+        return response_json
 
 
 async def calculate_distance_for_token(
