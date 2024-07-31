90862b5ad179393c26e126ea45985fa4805ec3ea
leviet0999 <leviet099898@gmail.com>
2024-07-31 13:19:38 +0700

    Update test.py

--- validator_orchestrator/app/checking/test.py
+++ validator_orchestrator/app/checking/test.py
@@ -2,10 +2,20 @@ import sys
 sys.path.append("../../") # go to parent dir
 
 import requests
-from app.checking.checking_functions import get_chat_data_validator_response
+from app.checking.checking_functions import get_chat_data_validator_response, check_text_result, get_chat_data_response
 import json
+import httpx
+from pydantic import BaseModel
+from typing import Dict, Union, Optional, Any, List
 
-url = 'http://localhost:11437/generate_text'
+class QueryResult(BaseModel):
+    formatted_response: Optional[Union[Dict[str, Any], List[Dict[str, Any]]]]
+    axon_uid: Optional[int]
+    response_time: Optional[float]
+    error_message: Optional[str]
+    failed_axon_uids: List[int] = []
+
+url = 'http://localhost:13320/generate_text'
 headers = {
     'accept': 'application/json',
     'Content-Type': 'application/json'
@@ -14,37 +24,43 @@ data = {
     "messages": [
         {
             "role": "user",
-            "content": ""
+            "content": "Write a short paragraph to describe something about yourself"
         }
     ],
     "seed": 0,
-    "temperature": 0.5,
-    "max_tokens": 30,
-    "number_of_logprobs": 1,
+    "temperature": 0.1,
+    "max_tokens": 50,
+    "number_of_logprobs": 5,
     "starting_assistant_message": True,
-    "top_p": 1,
+    "top_k": 5,
     "additionalProp1": {}
 }
 
 from asgiref.sync import async_to_sync
 
+
 @async_to_sync
 async def print_data(data):
-    return await get_chat_data_validator_response(endpoint=url, data=data)
+    return await get_chat_data_response(endpoint=url, data=data)
+
+# a = print_data(data)
+
+# with open("test.json", "w") as f:
+#     json.dump(a, f)
 
 
 # Open the file in read mode
-list_valid = []
+count = 0 
 with open('test.txt', 'r') as file:
     # Iterate over each line in the file
     for line in file:
         data['messages'][0]['content'] = "Write a short paragraph to describe: " + line.strip()
         a = print_data(data)
-        list_valid.append(a)
-
-
-# Write to a jsonl file
-with open('data_test.jsonl', 'w') as file:
-    for entry in list_valid:
-        # Convert the dictionary to a JSON string and write it to the file
-        file.write(json.dumps(entry) + '\n')
+        with open(f"test_data/{count}.json", "w") as f:
+            json.dump(a, f)
+        count += 1
+# # Write to a jsonl file
+# with open('data_test.jsonl', 'w') as file:
+#     for entry in list_valid:
+#         # Convert the dictionary to a JSON string and write it to the file
+#         file.write(json.dumps(entry) + '\n')
